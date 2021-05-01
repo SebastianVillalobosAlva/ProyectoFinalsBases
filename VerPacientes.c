e2e9be1081cc7826486e1ff255e603496609ad43
Sebas <s.villalobos.alva@gmail.com>
2021-05-01 14:22:31 -0500

    Added ModuloAdmin

---
+++ VerPacientes.c
@@ -0,0 +1,32 @@
+#include <mysql/mysql.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void VerPacientes(MYSQL *conU){
+    char enter[5];
+    unsigned int i;
+    MYSQL_RES *resPac;
+    MYSQL_ROW rowPac;
+
+    // sprintf(sql_statement,"SELECT * FROM PF_usuarios");
+    printf("\nEntro a VerUsuarios\n");
+    mysql_query(conU,"SELECT Nombrep, ApellidoPatp, ApellidoMatp FROM PF_pacientes");
+    resPac = mysql_store_result(conU);
+
+    while(rowPac = mysql_fetch_row(resPac)){
+        i = 0;
+        for(i=0; i < mysql_num_fields(resPac); i++){
+            if(rowPac[i] != NULL){
+                printf("%s", rowPac[i]);
+                printf(" ");
+            }
+            else{
+                printf(" \n");
+            } 
+        }
+        printf("\n");
+    }
+    mysql_free_result(resPac);
+    scanf(" %s", enter);
+}
