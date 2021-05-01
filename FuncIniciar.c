08b29489e01b3eba0ae77affe404589d1fcf626d
Sebas <s.villalobos.alva@gmail.com>
2021-04-30 22:32:26 -0500

    Added admin and user log in

--- FuncIniciar.c
+++ FuncIniciar.c
@@ -3,15 +3,28 @@
 #include <string.h>
 #include <stdlib.h>
 
+//Admin: name=Sebastian pss=1q2w3e4r5t
+//User: name=Luis pss=1234
+
 void Iniciar(MYSQL *conn, char *name, char *pss){
     char sql_statement[2048];
+    bool user;
+    bool admin = TRUE;
 
     sprintf(sql_statement, "SELECT * FROM PF_admins WHERE Nombrea = '%s' AND Contrasenaa = '%s'", name, pss);
     if(mysql_query(conn, sql_statement) !=0){
-        printf("Query failed  with the following message:\n");
-        printf("%s\n", mysql_error(conn));
-        exit(1);
+        admin = FALSE;
+        sprintf(sql_statement, "SELECT * FROM PF_admins WHERE Nombrea = '%s' AND Contrasenaa = '%s'", name, pss);
+        if(mysql_query(conn, sql_statement) !=0){
+            user = FALSE;
+            return;
+        }
+        user = TRUE;
     }
-
-    printf("User %s signed in\n", name);
+    if (admin){
+        printf("Admin %s, signed in", name);
+    }
+    if (user){
+        printf("User %s, signed in", name);
+    }
 }
