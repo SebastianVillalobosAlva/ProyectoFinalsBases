01de04002f17af11d23c90d7f419c572810f04f8
Sebas <s.villalobos.alva@gmail.com>
2021-05-01 00:50:30 -0500

    Added admin and user log in

--- FuncIniciar.c
+++ FuncIniciar.c
@@ -7,27 +7,28 @@
 //User: name=Luis pss=1234
 
 void Iniciar(MYSQL *conn, char *name, char *pss){
-    char sql_statement[100];
-    int mysqlStatus = 0;
-    unsigned int i = 0;
-    unsigned int num_fields;
-    MYSQL_RES *res;
-    MYSQL_ROW row;
-    MYSQL_FIELD *fields;
+    char sql_statement[100],sql_statement_u[100];
+    MYSQL_RES *resAdmin, *resUser;
+    MYSQL_ROW rowAdmin, rowUser;
 
     sprintf(sql_statement,"SELECT * FROM PF_admins WHERE Nombrea='%s' AND Contrasenaa='%s'", name, pss);
     mysql_query(conn,sql_statement);
-    res = mysql_store_result(conn);
+    resAdmin = mysql_store_result(conn);
+    rowAdmin = mysql_fetch_row(resAdmin);
 
-    while((row = mysql_fetch_row(res))){
-        if(row != NULL){
-            printf("Hay datos");
-        }
-        i = 0;
-        for(i=0; i<mysql_num_fields(res); i++){
-            if(row[i] != NULL){
-                printf("%s ", row[i]);
-            }
+    if(rowAdmin != NULL){
+        printf("Admin %s, signed in", rowAdmin[1]);
+        mysql_free_result(resAdmin);
+    }
+    else{
+        sprintf(sql_statement_u,"SELECT * FROM PF_usuarios WHERE Nombreu='%s' AND Contrasenau='%s'", name, pss);
+        mysql_query(conn,sql_statement_u);
+        resUser = mysql_store_result(conn);
+        rowUser = mysql_fetch_row(resUser);
+         if(rowUser != NULL){
+        printf("User %s, signed in", rowUser[1]);
+        mysql_free_result(resUser);
         }
     }
+
 }
