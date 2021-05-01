05a04dcd8db6ff96f9916dbe294fe0970d4aa243
Sebas <s.villalobos.alva@gmail.com>
2021-05-01 15:39:39 -0500

    Added AgregarUsuario

--- AgregarUsuario.c
+++ AgregarUsuario.c
@@ -4,27 +4,28 @@
 #include <stdlib.h>
 
 void AgregarUsuario(MYSQL *conU){
-    char Nombreu[25], ApellidoPatu[25], ApellidoMatu[25], Puesto[25], FechaNacu[15], Emailu[30], Contrasenau[30];
+    char Nombreu[40], ApellidoPatu[40], ApellidoMatu[40], Puesto[40], FechaNacu[40], Emailu[40], Contrasenau[40];
     char sql_statement[150], enter[10];
 
     printf("\nPor favor llena la informacion del usuario que quiere agregar:\n");
     printf("Nombre:");
-    scanf(" %s", Nombreu);
+    scanf("%s", Nombreu);
     printf("Apellido Paterno:");
-    scanf(" %s", ApellidoPatu);
+    scanf("%s", ApellidoPatu);
     printf("Apellido Materno:");
-    scanf(" %s", ApellidoMatu);
+    scanf("%s", ApellidoMatu);
     printf("Puesto:");
-    scanf(" %s", Puesto);
+    scanf("%s", Puesto);
     printf("Fecha de Nacimiento (2020-01-01):");
-    scanf(" %s", FechaNacu);
+    scanf("%s", FechaNacu);
     printf("Emailu:");
-    scanf(" %s", Emailu);
+    scanf("%s", Emailu);
     printf("Contaseña:");
-    scanf(" %s", Contrasenau);
+    scanf("%s", Contrasenau);
 
     sprintf(sql_statement, "INSERT INTO PF_usuarios (Nombreu, ApellidoPatu, ApellidoMatu, puesto, FechaNacu, Emailu, Contrasenau) VALUES('%s','%s','%s','%s','%s','%s','%s')", Nombreu, ApellidoPatu, ApellidoMatu, Puesto, FechaNacu, Emailu, Contrasenau );
     mysql_query(conU,sql_statement);
+
     // printf("\nSelecciona una tecla y aprieta enter\n");
     // scanf(" %s", enter);
 }
