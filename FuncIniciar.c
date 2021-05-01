e28a1446cc9ed0f3e7dee000ba319974508bb261
Sebas <s.villalobos.alva@gmail.com>
2021-04-30 22:55:04 -0500

    Added admin and user log in

--- FuncIniciar.c
+++ FuncIniciar.c
@@ -7,7 +7,7 @@
 //User: name=Luis pss=1234
 
 void Iniciar(MYSQL *conn, char *name, char *pss){
-    char sql_statement_admin[2048], sql_statement_user[2040];
+    char sql_statement_admin[2048], sql_statement_user[2048];
     int user;
     int admin;
 
@@ -15,9 +15,11 @@ void Iniciar(MYSQL *conn, char *name, char *pss){
 
     sprintf(sql_statement_admin, "SELECT * FROM PF_admins WHERE Nombrea = '%s' AND Contrasenaa = '%s'", name, pss);
     if(mysql_query(conn, sql_statement_admin) != 0){
+        printf("\nNo es admin\n");
         admin = 0;
         sprintf(sql_statement_user, "SELECT * FROM PF_usuarios WHERE Nombreu = '%s' AND Contrasenau = '%s'", name, pss);
         if(mysql_query(conn, sql_statement_user) != 0){
+            printf("\nNo es usuario\n");
             user = 0;
             printf("\n Nadie inició sesión");
             return;
