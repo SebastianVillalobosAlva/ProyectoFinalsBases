86cbe54b518a4daeac16cfcbe375982cff9e395e
Sebas <s.villalobos.alva@gmail.com>
2021-05-01 18:02:57 -0500

    Added ModificarUsuario

--- ModificarUsuario.c
+++ ModificarUsuario.c
@@ -5,7 +5,7 @@
 
 void ModificarUsuario(MYSQL *conU){
     char enter[5], sql_statement[2048], Nombre[25], ApellidoPat[25], ApellidoMat[25], FechaNac[25], Email[30];
-    int opcion, id;
+    int opcion = 0, id = 0;
     MYSQL_RES *resPac;
     MYSQL_ROW rowPac;
 
