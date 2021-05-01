6bdb9229940cf616516e6e778149b7d46ba5b8d7
Sebas <s.villalobos.alva@gmail.com>
2021-04-30 22:49:02 -0500

    Added admin and user log in

--- FuncIniciar.c
+++ FuncIniciar.c
@@ -7,24 +7,22 @@
 //User: name=Luis pss=1234
 
 void Iniciar(MYSQL *conn, char *name, char *pss){
-    char sql_statement[2048];
-    _Bool user = 1;
-    _Bool admin = 1;
+    char sql_statement_admin[2048], sql_statement_user[2040];
+    _Bool user;
+    _Bool admin;
 
-    sprintf(sql_statement, "SELECT * FROM PF_admins WHERE Nombrea = '%s' AND Contrasenaa = '%s'", name, pss);
-    if(mysql_query(conn, sql_statement) !=0){
+    sprintf(sql_statement_admin, "SELECT * FROM PF_admins WHERE Nombrea = '%s' AND Contrasenaa = '%s'", name, pss);
+    if(mysql_query(conn, sql_statement_admin) !=0){
         admin = 0;
-        sprintf(sql_statement, "SELECT * FROM PF_usuarios WHERE Nombreu = '%s' AND Contrasenau = '%s'", name, pss);
-        if(mysql_query(conn, sql_statement) !=0){
+        sprintf(sql_statement_user, "SELECT * FROM PF_usuarios WHERE Nombreu = '%s' AND Contrasenau = '%s'", name, pss);
+        if(mysql_query(conn, sql_statement_user) !=0){
             user = 0;
             return;
         }
         user = 1;
     }
     if (admin){
-        printf("Admin %s, signed in", name);
+        printf("Admin %s, signed in\n", name);
     }
-    if (user){
-        printf("User %s, signed in", name);
-    }
+
 }
