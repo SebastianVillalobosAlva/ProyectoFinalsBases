b54feff9030ba429d3d717ca0f235c5ab3895a38
Sebas <s.villalobos.alva@gmail.com>
2021-05-07 17:21:17 -0500

    Added registro de busquedas

--- BuscarUsuario.c
+++ BuscarUsuario.c
@@ -95,9 +95,7 @@ void BuscarUsuario(MYSQL *conU, char *name){
             printf("\n");
         }
         sprintf(sql_statement_2, "Se busco al usuario con el apellido %s", ApellidoPat);
-        printf("%s", sql_statement_2);
         sprintf(sql_statement_3, "INSERT INTO PF_registrobus (registro, NombreU) VALUES ('%s', '%s')", sql_statement_2, name);
-        printf("%s", sql_statement_3);
         mysql_query(conU,sql_statement_3);
         break;
 
@@ -128,9 +126,8 @@ void BuscarUsuario(MYSQL *conU, char *name){
             printf("\n");
         }
         sprintf(sql_statement_2, "Se busco al usuario con el nombre %s y el apellido %s", Nombre, ApellidoPat);
-        printf("%s", sql_statement_2);
         sprintf(sql_statement_3, "INSERT INTO PF_registrobus (registro, NombreU) VALUES ('%s', '%s')", sql_statement_2, name);
-        printf("%s", sql_statement_3);
+        mysql_query(conU,sql_statement_3);
         break;
     }
     /* Liberamos el resultado */
