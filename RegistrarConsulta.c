f1a68d51ab48041de41b2c91ee51b258fe3baefd
Sebas <s.villalobos.alva@gmail.com>
2021-05-08 23:26:57 -0500

    Making Registrar Consulta

--- RegistrarConsulta.c
+++ RegistrarConsulta.c
@@ -23,7 +23,7 @@ void RegistrarConsulta(MYSQL *conU, char *name, char *pss){
     resUser = mysql_store_result(conU);
     rowUser = mysql_fetch_row(resUser);
     IDpaciente = atoi(rowUser[0]);
-    printf("\nNumero de IDpaciente: %i\n", IDpaciente);
+    printf("\nNumero de IDpaciente: %i", IDpaciente);
     mysql_free_result(resUser);
 
     /* Obtener ID del usuario */
@@ -32,7 +32,7 @@ void RegistrarConsulta(MYSQL *conU, char *name, char *pss){
     resUser = mysql_store_result(conU);
     rowUser = mysql_fetch_row(resUser);
     IDusuario = atoi(rowUser[0]);
-    printf("\nNumero de IDusuario: %i\n", IDusuario);
+    printf("\nNumero de IDusuario: %i\n\n", IDusuario);
     mysql_free_result(resUser);
 
     /* Obtener ID del diagnostico */
@@ -53,9 +53,9 @@ void RegistrarConsulta(MYSQL *conU, char *name, char *pss){
         printf("\n");
     }
     mysql_free_result(resUser);
-    printf("Escoge el diagnostico (Numero): ");
+    printf("\nEscoge el diagnostico (Numero): ");
     scanf(" %i", &IDdiagnostico);
-    printf("\nEl IDdiagnostico es: %i\n", IDdiagnostico);
+    printf("\nEl IDdiagnostico es: %i\n\n", IDdiagnostico);
 
     mysql_query(conU,"SELECT medicamento, IDmedicina FROM PF_meds");
     resUser = mysql_store_result(conU);
@@ -74,9 +74,9 @@ void RegistrarConsulta(MYSQL *conU, char *name, char *pss){
         printf("\n");
     }
     mysql_free_result(resUser);
-    printf("Escoge la medicina (Numero): ");
+    printf("\nEscoge la medicina (Numero): ");
     scanf(" %i", &IDmedicina);
-    printf("\nEl IDmedicina es: %i\n", IDmedicina);
+    printf("\nEl IDmedicina es: %i\n\n", IDmedicina);
 
     mysql_query(conU,"SELECT enfermedad, IDenfermedad FROM PF_enfermedad");
     resUser = mysql_store_result(conU);
@@ -95,7 +95,7 @@ void RegistrarConsulta(MYSQL *conU, char *name, char *pss){
         printf("\n");
     }
     mysql_free_result(resUser);
-    printf("Escoge la enfermedad (Numero): ");
+    printf("\nEscoge la enfermedad (Numero): ");
     scanf(" %i", &IDenfermedad);
     printf("\nEl IDenfermedad es: %i\n", IDenfermedad);
 
