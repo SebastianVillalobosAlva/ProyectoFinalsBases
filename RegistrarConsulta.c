3bbc031a84d8506d28cab6f390c761159d4b91b2
Sebas <s.villalobos.alva@gmail.com>
2021-05-08 20:43:40 -0500

    Addin RegistrarConsulta

--- RegistrarConsulta.c
+++ RegistrarConsulta.c
@@ -4,21 +4,21 @@
 #include <stdlib.h>
 
 void RegistrarConsulta(MYSQL *conU){
-    char Nombre[40], email[40], sql_statement[5000];
-    unsigned int i;
-    printf("\nIngresa el nombre del paciente: ");
-    scanf("%s", Nombre);
-    printf("\nIngresa el email del paciente: ");
-    scanf("%s", email);
+    // char Nombre[40], email[40], sql_statement[5000];
+    // unsigned int i;
+    // printf("\nIngresa el nombre del paciente: ");
+    // scanf("%s", Nombre);
+    // printf("\nIngresa el email del paciente: ");
+    // scanf("%s", email);
 
     MYSQL_RES *resUser;
     MYSQL_ROW rowUser;
 
-    sprintf(sql_statement,"SELECT IDpaciente FROM PF_pacientes WHERE Nombre = '%s' AND Email = '%s'", Nombre, email);
-    mysql_query(conU,sql_statement);
+    // sprintf(sql_statement,"SELECT IDpaciente FROM PF_pacientes WHERE Nombre = '%s' AND Email = '%s'", Nombre, email);
+    mysql_query(conU,"SELECT IDpaciente FROM PF_pacientes WHERE Nombre = 'Jaime' AND Email = 'jasamelu@gmail.com'");
     resUser = mysql_store_result(conU);
     rowUser = mysql_fetch_row(resUser);
     printf("%s", rowUser[0]);
-    printf("%s", rowUser[0]);
+    printf("%s", rowUser[1]);
     mysql_free_result(resUser);
 }
