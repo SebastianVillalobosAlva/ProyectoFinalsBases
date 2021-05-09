3d5fb14b4bd92404da862a223fd1b69db79bafd5
Sebas <s.villalobos.alva@gmail.com>
2021-05-09 18:47:38 -0500

    Finishing RegistrarConsulta

--- RegistrarConsulta.c
+++ RegistrarConsulta.c
@@ -87,7 +87,7 @@ void RegistrarConsulta(MYSQL *conU, char *name, char *pss){
     scanf("%i", &costo);
     // costo = atof(costo);
 
-    sprintf(sql_statement, "INSERT INTO PF_consultas (descripcion, costo, IDpaciente, IDusuario, IDmedicina, IDenfermedad) VALUES ('%s','%i','%i','%i','%i','%i')", descripcion, costo, IDpaciente, IDusuario, IDmedicina, IDenfermedad);
+    sprintf(sql_statement, "INSERT INTO PF_consultas (descripcionc, costo, IDpaciente, IDusuario, IDmedicina, IDenfermedad) VALUES ('%s','%i','%i','%i','%i','%i')", descripcion, costo, IDpaciente, IDusuario, IDmedicina, IDenfermedad);
     mysql_query(conU,sql_statement);
 
     printf("\nSelecciona una tecla y aprieta enter\n");
