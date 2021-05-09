3b9ca76078c1af4840bb14de33b4dcb0db517089
Sebas <s.villalobos.alva@gmail.com>
2021-05-08 20:32:18 -0500

    Addin RegistrarConsulta

--- RegistrarConsulta.c
+++ RegistrarConsulta.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 void RegistrarConsulta(MYSQL *conU){
-    char Nombre[40], email[40], sql_statement[5000];
+    char Nombre[40], email[], sql_statement[5000];
     unsigned int i;
     printf("\nIngresa el nombre del paciente: ");
     scanf("%s", Nombre);
@@ -14,14 +14,17 @@ void RegistrarConsulta(MYSQL *conU){
     MYSQL_RES *resUser;
     MYSQL_ROW rowUser;
 
-    sprintf(sql_statement,"SELECT IDpaciente FROM PF_paciente WHERE Nombre ='%s' AND Email = '%s'", Nombre, email);
+    sprintf(sql_statement,"SELECT IDpaciente FROM PF_pacientes WHERE Nombre = '%s' AND Email = '%s'", Nombre, email);
     mysql_query(conU,sql_statement);
+    resUser = mysql_store_result(conU);
+    
     while(rowUser = mysql_fetch_row(resUser)){
         i = 0;
         for(i=0; i < mysql_num_fields(resUser); i++){
             if(rowUser[i] != NULL){
                 printf("%s", rowUser[i]);
                 printf(" ");
+                printf("\ni vale: %i", i);
             }
             else{
                 printf(" \n");
